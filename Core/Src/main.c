328489993b5a465f303a21e47a68a338ba6c0935
Elena Justo <elenaj@protonmail.ch>
2024-04-25 12:14:01 +1000

    modified TIM3 ARR and PSC to match event cycle of 50hz

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -348,9 +348,9 @@ static void MX_TIM3_Init(void)
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 0;
+  htim3.Init.Prescaler = 319;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 65535;
+  htim3.Init.Period = 999;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
@@ -554,7 +554,7 @@ static void MX_GPIO_Init(void)
 
 	/* Setup timer */
 	void timerSetup(void){
-
+		HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1); // Using HAL to Enable PWM
 	}
 
 	/* Motor setup */
@@ -569,6 +569,11 @@ static void MX_GPIO_Init(void)
 
 		// Vary the duty cycle in a for-loop between 1ms to 2ms in the capture/compare
 		// register to sweep the servo.
+	}
+
+	/* Motor control */
+	void motorControl(adcValue){
+		__HAL_TIM_SET_COMPARE (&htim3, TIM_CHANNEL_1, adcValue); // Interpolate ADC values to PWM range
 	}
 
 	/* I2C Scanner Script */
